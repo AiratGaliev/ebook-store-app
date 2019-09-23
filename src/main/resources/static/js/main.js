5602b4b3ae4a07bfb79b50b7b5e4fbb3d23ad8d9
Airat Galiev <airat.galiev@gmail.com>
2019-09-23 17:00:30 +0300

    добавлена карусель, изменена навигация

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll('.navbar-burger'), 0);
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
 
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
+    $navbarBurgers.forEach(el => {
       el.addEventListener('click', () => {
 
         // Get the target from the "data-target" attribute
@@ -22,4 +23,35 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  bulmaCarousel.attach('.hero-carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    pagination: false,
+    effect: 'fade',
+    loop: true
+  });
 });
+
+// ready(function () {
+//   bulmaCarousel.attach('#slider', {
+//     slidesToScroll: 1,
+//     slidesToShow: 4,
+//   });
+//   bulmaCarousel.attach('#slider1', {
+//     slidesToScroll: 1,
+//     slidesToShow: 3,
+//     infinite: true
+//   });
+//   bulmaCarousel.attach('#slider2', {
+//     slidesToScroll: 1,
+//     slidesToShow: 3,
+//     loop: true
+//   });
+//   bulmaCarousel.attach('.hero-carousel', {
+//     slidesToScroll: 1,
+//     slidesToShow: 1,
+//     pagination: false,
+//     effect: 'fade',
+//     loop: true
+//   });
+// });
